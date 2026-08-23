553b0dc8dae76135cc9f1a02c294cb4f4d34a046
hypercomrade <mckaylbing@gmail.com>
2026-08-23 10:45:18 -0600

    Add init script way to tell what os you are using

---
+++ mikhail_init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# File to download basic software becuase I'm lazy
+
+if cat /etc/os-release | grep -q "Arch"; then
+	echo "Arch"
+	sudo pacman -Syy
+
+elif cat /etc/os-release | grep -q "Ubuntu"; then
+	echo "Ubuntu"
+
+else
+	echo "Null"
+fi
