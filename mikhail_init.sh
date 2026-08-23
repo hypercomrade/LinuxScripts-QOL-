2b4430a8b2b96eb2572cab051b38b7837112e8a4
hypercomrade <mckaylbing@gmail.com>
2026-08-23 11:19:22 -0600

    Refine init file

--- mikhail_init.sh
+++ mikhail_init.sh
@@ -10,6 +10,7 @@ if cat /etc/os-release | grep -q "Arch"; then
 	sudo pacman -S --noconfirm fastfetch
 	sudo pacman -S --noconfirm vlc
 	fastfetch
+	# Commands to change history length
 
 elif cat /etc/os-release | grep -q "Ubuntu"; then
 	echo "Ubuntu"
